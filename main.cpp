0177adc7fc74d3e4debeaa462d6c2499b34e285d
BimboYU <ali.hamza9321@gmail.com>
2024-09-16 13:05:31 +0500

    Quiz done

--- main.cpp
+++ main.cpp
@@ -5,10 +5,29 @@ using namespace std;
 
 // This function performs a stable selection sort on a vector of any type.
 // `pos` specifies the index to sort by if sorting a vector of pairs.
+
+
+// Implementation of stableSelectionSort. Due to absence of Equal keys, output is skewed.
+// Correct Output: (1, 3) (2, 2) (2, 1) (3, 1)
 template <typename T>
 void stableSelectionSort(vector<T>& a) {
-    
+    int n = a.size();
+    for (int i = 0; i < n - 1; ++i) {
+        int minIndex = i;
+        for (int j = i + 1; j < n; ++j) {
+            if (a[j] < a[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        T minElement = a[minIndex];
+        for (int k = minIndex; k > i; --k) {
+            a[k] = a[k - 1];
+        }
+        a[i] = minElement;
+    }
 }
+
 
 // Unstable Selection Sort given in slides
 template <typename T>
@@ -34,7 +53,7 @@ int main() {
     cout << endl;
 
     // Sort by the second element of the pairs
-    selectionSort(arr);
+    stableSelectionSort(arr);
 
     // Print sorted array
     cout << "Sorted array by the first element:" << endl;
